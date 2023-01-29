9e16991c0db88221d44fc2d5366433882a38c068
Michael_Zakharov <mihzar20080405@gmail.com>
2023-01-29 12:56:30 +0400

    decision_task1_seminar2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿ Console.WriteLine("Начало работы по семинару2");
+ // напишите программу, котрорая принимает на вход трехзнач. число и удаляет вторую цифру этого числа
+
+ int CutNumber(int number)
+ {
+    int firstInt =  number / 10;
+    int secondInt = number % 10;
+    int maxInt = firstInt;
+
+    if(firstInt < secondInt)
+        maxInt = secondInt;
+    return maxInt;
+     
+ }
+
+ int number1 = new Random().Next(10, 100);
+int result = CutNumber(number1);
+ Console.WriteLine(result);
